34198d9c4641c1882f206e219cc5cbc26e1a84b7
Keiji O <meekunn@gmail.com>
2023-04-23 03:19:39 -0700

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+
+
+
+//array shuffle function by ashleedawg, the updated version of durstendfeld shuffle
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
